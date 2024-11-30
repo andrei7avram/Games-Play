4675775229046d3addfa7f977331e02e7c4a8728
MagicMerlin132 <M.H.J.STROEVE@STUDENT.TUE.NL>
2024-11-30 12:56:00 +0100

    Added camera switching
    It is now possible to switch to cameras that look at objects (pc screen, paper) by clicking on it. By clicking on anything but the object,  it is possible to return to the main camera

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject CameraMain;
+    public GameObject CameraPC;
+    public GameObject CameraPaper;
+
+    private Camera playerCamera;
+
+    void Start()
+    {
+        // Initialize cameras
+        CameraMain.SetActive(true);
+        CameraPC.SetActive(false);
+        CameraPaper.SetActive(false);
+
+        // Initialize the playerCamera to the main camera
+        playerCamera = CameraMain.GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        // Check for mouse input
+        if (Input.GetMouseButtonDown(0)) // Left mouse button
+        {
+            DetectObject();
+        }
+    }
+
+    void DetectObject()
+    {
+        // Raycast from the center of the screen
+        Ray ray = playerCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            GameObject hitObject = hit.collider.gameObject;
+
+            // Check the tag of the hit object
+            if (hitObject.CompareTag("PC"))
+            {
+                Camera2();
+            }
+            else if (hitObject.CompareTag("Paper"))
+            {
+                Camera3();
+            }
+            else
+            {
+                Camera1();
+            }
+        }
+    }
+
+    void Camera1()
+    {
+        CameraMain.SetActive(true);
+        CameraPC.SetActive(false);
+        CameraPaper.SetActive(false);
+
+        // Update playerCamera reference
+        playerCamera = CameraMain.GetComponent<Camera>();
+    }
+
+    void Camera2()
+    {
+        CameraMain.SetActive(false);
+        CameraPC.SetActive(true);
+        CameraPaper.SetActive(false);
+
+        // Update playerCamera reference
+        playerCamera = CameraPC.GetComponent<Camera>();
+    }
+
+    void Camera3()
+    {
+        CameraMain.SetActive(false);
+        CameraPC.SetActive(false);
+        CameraPaper.SetActive(true);
+
+        // Update playerCamera reference
+        playerCamera = CameraPaper.GetComponent<Camera>();
+    }
+}
