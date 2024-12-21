6a5f0966237a3578e9e7a92e06b5043a311dd314
Furbzzalicious <zmonaghan@hotmail.com>
2024-12-21 17:50:21 +0100

    Fixed Errors with prefab scripts

--- Assets/Fries and Seagull/Interior 01/Scripts/Utility/Inspector/IgnoreInInspectorAttribute.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Fries.Interior_01.Utility {
-    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
-    public class IgnoreInInspectorAttribute : Attribute {
-        
-    }
-}
--- Assets/Fries and Seagull/Interior 01/Scripts/Utility/TurnOnAble.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-namespace Fries.Interior_01.Utility {
-    public class TurnOnAble : MonoBehaviour {
-        
-    }
-}
