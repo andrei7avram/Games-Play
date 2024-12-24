7f25c022154e0ae4e7ee5b924316dea10d3da6c7
Leonidas Hadjiyiannis <l.hadjiyiannis@student.tue.nl>
2024-12-24 15:54:43 +0200

    Bobble Head + Sticky Note + Menu

---
+++ Assets/Scripts/HeadMovement.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class HeadMovement : MonoBehaviour
+{
+    private Vector3 originalPosition;
+    public float movementDistance = 0.5f;
+    public float movementSpeed = 2f;
+    public int numberOfMoves = 3;
+
+    void Start()
+    {
+        originalPosition = transform.localPosition;
+    }
+
+    void OnMouseDown()
+    {
+        StopAllCoroutines();
+        StartCoroutine(MoveHead());
+    }
+
+    private System.Collections.IEnumerator MoveHead()
+    {
+        for (int i = 0; i < numberOfMoves; i++)
+        {
+            yield return MoveToPosition(originalPosition + Vector3.left * movementDistance);
+            yield return MoveToPosition(originalPosition + Vector3.right * movementDistance);
+        }
+        yield return MoveToPosition(originalPosition);
+    }
+
+    private System.Collections.IEnumerator MoveToPosition(Vector3 targetPosition)
+    {
+        while (Vector3.Distance(transform.localPosition, targetPosition) > 0.01f)
+        {
+            transform.localPosition = Vector3.Lerp(transform.localPosition, targetPosition, Time.deltaTime * movementSpeed);
+            yield return null;
+        }
+        transform.localPosition = targetPosition;
+    }
+}
