a77dd1cfce1efc4335b9b39feef55809b5e72487
MagicMerlin132 <M.H.J.STROEVE@STUDENT.TUE.NL>
2024-12-28 12:26:44 +0100

    Programmed studies and created help page
    I have programmed a way to switch between the studies in the studies page. I have designed the help page.

---
+++ Assets/Scripts/TextureSwitcher.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureSwitcher : MonoBehaviour
+{
+    public Texture ComputerScience;
+    public Texture AppliedPhysics;
+    public Texture IndustrialDesign;
+    public Texture Architecture;
+    public Texture MedicalSciences;
+    public Texture ElectricalEngineering;
+    public Texture ChemicalEngineering;
+    public Texture Sustainable;
+    public Texture Psychology;
+
+    private Texture[] textures;
+    private int currentIndex = 0; // Index to keep track of the current texture
+    private Renderer rend; // Renderer component of the GameObject
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+
+        // Populate the textures array with the named textures
+        textures = new Texture[]
+        {
+            ComputerScience, AppliedPhysics, IndustrialDesign, Architecture, MedicalSciences,
+            ElectricalEngineering, ChemicalEngineering, Sustainable, Psychology
+        };
+
+        // Set the initial texture
+        rend.material.mainTexture = textures[currentIndex];
+        rend.material.SetTexture("_EmissionMap", textures[currentIndex]);
+        rend.material.EnableKeyword("_EMISSION"); // Enable emission keyword
+    }
+
+    void Update()
+    {
+        // Check for left arrow key input
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            currentIndex--;
+            if (currentIndex < 0)
+                currentIndex = textures.Length - 1;
+            rend.material.mainTexture = textures[currentIndex];
+            rend.material.SetTexture("_EmissionMap", textures[currentIndex]);
+        }
+
+        // Check for right arrow key input
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            currentIndex++;
+            if (currentIndex >= textures.Length)
+                currentIndex = 0;
+            rend.material.mainTexture = textures[currentIndex];
+            rend.material.SetTexture("_EmissionMap", textures[currentIndex]);
+        }
+    }
+}
