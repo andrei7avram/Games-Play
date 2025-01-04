33696729abb4ea7249c8d843e8b7fbee4fe1e9f5
Leonidas Hadjiyiannis <l.hadjiyiannis@student.tue.nl>
2025-01-04 11:27:28 +0200

    Fade to Black animation between scenes
    Left click on the Fade to black #1 scene to go to the Fade to black #2 scene (with knocking sound)

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = 1f;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            LoadNextLevel();
+        }
+    }
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+
+        transition.SetTrigger("Start");
+
+        audioSource.Play();
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+}
