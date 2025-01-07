aad513662542f385ec003197b472d9042fd35ceb
andrei7avram <66694499+andrei7avram@users.noreply.github.com>
2025-01-07 21:02:44 +0100

    Fixed Clickable PC + merged level incrementation into main scene

--- Assets/PC.cs
+++ Assets/PC.cs
@@ -7,6 +7,77 @@ public class PC : MonoBehaviour
 {
     public GameObject[] panels = new GameObject[5];
 
+    public Camera targetCamera; 
+    public Camera currentCamera; 
+    public Canvas worldSpaceCanvas;
+    public PersonaManager personaManager;
+
+    Ray ray;
+
+    public bool isTargetCameraActive = false;
+
+    public void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !personaManager.isLevelComplete) 
+        {
+            if (!isTargetCameraActive) {
+                ray = currentCamera.ScreenPointToRay(Input.mousePosition);
+            }else {
+                ray = targetCamera.ScreenPointToRay(Input.mousePosition);
+            }
+             
+            RaycastHit hit;
+            Debug.DrawRay(ray.origin, ray.direction * 1000000, Color.red, 2f);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                
+                if (hit.collider.gameObject.CompareTag("Monitor"))
+                {   
+                    isTargetCameraActive = true;
+                    SwitchToTargetCamera();
+                    worldSpaceCanvas.worldCamera = targetCamera;
+                    ray = targetCamera.ScreenPointToRay(Input.mousePosition);
+                    Debug.Log("Switching to target camera");
+                   
+                }else
+                {   
+                    isTargetCameraActive = false;
+                    SwitchToCurrentCamera();
+                    worldSpaceCanvas.worldCamera = currentCamera;
+                    ray = currentCamera.ScreenPointToRay(Input.mousePosition);
+                    Debug.Log(hit.collider.gameObject.name);
+                }
+            }
+        }
+    }
+
+    void SwitchToTargetCamera()
+    {
+        if (currentCamera != null)
+        {
+            currentCamera.gameObject.SetActive(false); 
+        }
+
+        if (targetCamera != null)
+        {
+            targetCamera.gameObject.SetActive(true); 
+        }
+    }
+
+    void SwitchToCurrentCamera()
+    {
+        if (targetCamera != null)
+        {
+            targetCamera.gameObject.SetActive(false); 
+        }
+
+        if (currentCamera != null)
+        {
+            currentCamera.gameObject.SetActive(true); 
+        }
+    }
+
     public void Button()
     {
         Debug.Log("Button pressed");
