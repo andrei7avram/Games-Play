ef33920c82d40e069aa012cc020be359c60ed8ea
Leonidas Hadjiyiannis <l.hadjiyiannis@student.tue.nl>
2025-01-11 18:00:04 +0100

    Person 4 Dialogue

--- Assets/SyncStateWithPerson.cs
+++ Assets/SyncStateWithPerson.cs
@@ -6,10 +6,8 @@ public class SyncStateWithPerson : MonoBehaviour
 
     void Update()
     {
-        // Check if person2 is assigned
         if (person2 != null)
         {
-            // Enable or disable this GameObject based on the active state of person2
             gameObject.SetActive(person2.activeSelf);
         }
     }
